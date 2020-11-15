8381042e10392f5b2ac59f7a9cef99fcfa3e5d55
Achintya01313 <72142052+Achintya01313@users.noreply.github.com>
2020-11-15 16:52:59 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master (4)/MatterJSBoilerPlate-master/bob.js
@@ -0,0 +1,11 @@
+class Bob{
+
+    constructor(x,y,width,height);
+    super(x,y,width,height);
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+
+
+
+}  
